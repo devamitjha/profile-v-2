bc1f2ab2179b3c1ce2d12559e88d22a4a80242b7
amit <amit.jha@candere.com>
2024-06-25 17:10:11 +0530

    admin

--- src/admin/Admin_Layout.js
+++ src/admin/Admin_Layout.js
@@ -2,16 +2,26 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import AdminMenu from "../components/dashboard/AdminMenu";
 import AdminFooter from "../components/dashboard/AdminFooter";
+import "./admin.scss";
 
 
 
 const Admin_Layout = () => {
   return (
-    <>
-        <AdminMenu/>
-        <Outlet />
-        <AdminFooter/>
-    </>
+    <div className="section-admin">
+      <div className="section-admin-menu">
+        <AdminMenu />
+      </div>
+      <div className="section-admin-content">
+        <div className="section-admin-outlet">
+          <Outlet />
+        </div>
+        <div className="section-admin-footer">
+          <AdminFooter />
+        </div>
+      </div>
+
+    </div>
   )
 }
 
--- src/components/dashboard/AdminMenu.js
+++ src/components/dashboard/AdminMenu.js
@@ -1,45 +1,48 @@
-import React from 'react'
+import React from 'react';
+import "./dashboard.scss";
 
 const AdminMenu = () => {
-  return (
-    <ul>
-        <li>
-            <div>Post</div>
+    return (
+        <div className="admin-nav">
             <ul>
-                <li>All Posts</li>
-                <li>Add New</li>
-                <li>Categories</li>
-                <li>Tags</li>
+                <li className="active">
+                    <div className="active">Post</div>
+                    <ul>
+                        <li>All Posts</li>
+                        <li>Add New</li>
+                        <li>Categories</li>
+                        <li>Tags</li>
+                    </ul>
+                </li>
+                <li>
+                    <div>Media</div>
+                    <ul>
+                        <li>Library</li>
+                        <li>Add New</li>
+                    </ul>
+                </li>
+                <li>
+                    <div>Comments</div>
+                    <ul>
+                        <li>All</li>
+                        <li>Mine</li>
+                        <li>Pending</li>
+                        <li>Approved</li>
+                    </ul>
+
+                </li>
+                <li>
+                    <div>Users</div>
+                    <ul>
+                        <li>All User</li>
+                        <li>Add New</li>
+                        <li>Your Profile</li>
+                    </ul>
+
+                </li>
             </ul>
-        </li>
-        <li>
-            <div>Media</div>
-            <ul>
-                <li>Library</li>
-                <li>Add New</li>
-            </ul>
-        </li>
-        <li>
-            <div>Comments</div>
-            <ul>
-                <li>All</li>
-                <li>Mine</li>
-                <li>Pending</li>
-                <li>Approved</li>
-            </ul>
-            
-        </li>
-        <li>
-            <div>Users</div>
-            <ul>
-                <li>All User</li>
-                <li>Add New</li>
-                <li>Your Profile</li>
-            </ul>
-            
-        </li>
-    </ul>
-  )
+        </div>
+    )
 }
 
 export default AdminMenu
